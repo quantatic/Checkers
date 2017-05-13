77fbcb2fd903c8a05ec3350ef6c9a57ff45aa44b
quixotrykd <nadiasggeb001@gmail.com>
2017-05-13 17:25:30 -0400

    Added some more code that handles moving pieces. Also refactored to use a piece type Enum instead of an int

--- src/Board.java
+++ src/Board.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Arc2D;
 import java.awt.geom.Ellipse2D;
+import java.util.Arrays;
 
 /**
  * Created by Aidan on 5/13/2017.
@@ -16,7 +17,7 @@ public class Board extends JPanel{
 
     private final int SQUARE_SIZE = 64;
     private final int CHECKER_SIZE = 40;
-    private final int[][] BOARD = new int[8][8]; //access as board[y][x]. 0 for none, -1 for black, 1 for white
+    private final Piece[][] BOARD = new Piece[8][8]; //access as board[y][x]. 0 for none, -1 for black, 1 for white
     private final int WIDTH = BOARD[0].length;
     private final int HEIGHT = BOARD.length;
 
@@ -29,6 +30,7 @@ public class Board extends JPanel{
 
     private int selectedX = -1;
     private int selectedY = -1;
+    private int selectedPiece;
 
     private final int FPS = 60;
 
@@ -50,13 +52,17 @@ public class Board extends JPanel{
         setDoubleBuffered(true);
         setFocusable(true);
 
+        for(Piece[] i : BOARD){ //use a for each and Arrays.fill to initialize the BOARD with the empty Enum
+            Arrays.fill(i, Piece.EMPTY);
+        }
+
         for(int x = 0; x < WIDTH; x++){
             for(int y = 0; y < HEIGHT; y++){
                 if((x + y) % 2 == 1){
                     if(y < 3){
-                        BOARD[y][x] = -1;
+                        BOARD[y][x] = Piece.BLACK;
                     }else if(y > 4){
-                        BOARD[y][x] = 1;
+                        BOARD[y][x] = Piece.WHITE;
                     }
                 }
             }
@@ -64,14 +70,26 @@ public class Board extends JPanel{
         addMouseListener(new MouseAdapter(){
             @Override
             public void mousePressed(MouseEvent e){
-                selectedX = e.getX() / SQUARE_SIZE;
-                selectedY = e.getY() / SQUARE_SIZE;
+                if(selectedX == -1 && selectedY == -1){
+                    selectedX = e.getX() / SQUARE_SIZE;
+                    selectedY = e.getY() / SQUARE_SIZE;
+                    if(BOARD[selectedY][selectedX] == Piece.EMPTY){
+                        selectedX = -1;
+                        selectedY = -1;
+                    }
+                }else{
+                    int moveToX = e.getX() / SQUARE_SIZE;
+                    int moveToY = e.getY() / SQUARE_SIZE;
+                    attemptMove(selectedX, selectedY, moveToX, moveToY);
+                    selectedX = -1;
+                    selectedY = -1;
+                }
             }
 
             @Override
             public void mouseReleased(MouseEvent e){
-                selectedX = -1;
-                selectedY = -1;
+//                selectedX = -1;
+//                selectedY = -1;
             }
         });
     }
@@ -117,21 +135,40 @@ public class Board extends JPanel{
             for(int y = 0; y < HEIGHT; y++){
                 Ellipse2D.Double circle = new Ellipse2D.Double(1.0 * x * SQUARE_SIZE + 0.5 * SQUARE_SIZE - 0.5 * CHECKER_SIZE, 1.0 * y * SQUARE_SIZE + 0.5 * SQUARE_SIZE - 0.5 * CHECKER_SIZE, CHECKER_SIZE, CHECKER_SIZE);
                 switch(BOARD[y][x]){
-                    case 1:
+                    case WHITE:
                         g2d.setColor(WHITE_CHECKER);
                         break;
-                    case -1:
+                    case BLACK:
                         g2d.setColor(BLACK_CHECKER);
                         break;
                     default:
-                        assert BOARD[y][x] == 0;
+                        assert BOARD[y][x] == Piece.EMPTY;
                         break;
                 }
 
-                if(BOARD[y][x] != 0){
+                if(BOARD[y][x] != Piece.EMPTY){
                     g2d.fill(circle);
                 }
             }
         }
+    }
+
+    private boolean isValidMove(int x, int y){
+        return (BOARD[y][x] == Piece.EMPTY) && (((x + y) % 2) == 1); //return true if the space is empty and it is a black square, and false otherwise
+    }
+
+    private boolean attemptMove(int startX, int startY, int finalX, int finalY){
+        if(BOARD[finalY][finalX] == Piece.EMPTY && (finalX + finalY) % 2 == 1){
+            switch(BOARD[startY][startX]){
+                case WHITE:
+                    break;
+                case BLACK:
+                    break;
+                default:
+                    System.out.println("Check attemptMove(). You are trying to move from a square with no piece?");
+            }
+        }
+
+        return false;
     }
 }
---
+++ src/Piece.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Aidan on 5/13/2017.
+ */
+public enum Piece{
+    BLACK,
+    WHITE,
+    EMPTY;
+}
